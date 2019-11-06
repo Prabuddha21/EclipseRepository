dadd20ebfe0f340af2d6f54f9dc532eddb10dbfd
Prabuddha21 <prabuddhaalahakoon@yahoo.com>
2019-11-06 12:45:37 +0530

    Added constructors

--- sample-git-project/src/com/test/app/User.java
+++ sample-git-project/src/com/test/app/User.java
@@ -5,6 +5,16 @@ public class User {
 	private String name;
 	private int age;
 
+	public User() {
+		super();
+	}
+
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
 	public String getName() {
 		return name;
 	}
