789f6b6855861b0a8902df0d049029b0a1ddb941
Prabuddha21 <prabuddhaalahakoon@yahoo.com>
2019-11-06 12:25:31 +0530

    Added getter and setter for age

--- sample-git-project/src/com/test/app/User.java
+++ sample-git-project/src/com/test/app/User.java
@@ -12,4 +12,14 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 }
