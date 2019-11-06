649ca13f91e28c8cb4e238c43b54ec215a43af93
Prabuddha21 <prabuddhaalahakoon@yahoo.com>
2019-11-06 12:17:27 +0530

    Added getters and setters

--- sample-git-project/src/com/test/app/User.java
+++ sample-git-project/src/com/test/app/User.java
@@ -3,4 +3,12 @@ package com.test.app;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
